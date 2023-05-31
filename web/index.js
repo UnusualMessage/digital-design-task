54f0b39183ff998bf86d2ecbefb8039fc541a2da
Nikita Kotenko <nikita_kot2@mail.ru>
2023-05-31 21:53:16 +0300

    fix: Add json request

--- web/index.js
+++ web/index.js
@@ -1,16 +1,25 @@
 const select = document.querySelector(".header-title .numbers-api-select");
 const form = document.querySelector(".header-title .numbers-api");
 const text = document.querySelector(".header-title .numbers-api-text");
+const title = document.querySelector(".header-title .numbers-api-number");
 
 let currentType = "date";
 
-const updateText = async (type) => {
-    text.innerHTML = await getFact(currentType);
+const updateText = async () => {
+    const response = await getFact(currentType);
+
+    if (response.date) {
+        title.innerHTML = `${response.date} ${response.number}`
+    } else {
+        title.innerHTML = `${response.number}`
+    }
+
+    text.innerHTML = response.text;
 }
 
 const getFact = async (type) => {
-    const response = await fetch(`http://numbersapi.com/random/${type}`);
-    return await response.text();
+    const response = await fetch(`http://numbersapi.com/random/${type}?json`);
+    return await response.json();
 }
 
 const onSubmit = async (event) => {
